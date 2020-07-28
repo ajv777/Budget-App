e7b7f08dc90784acb78794076edc5785a643901c
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-28 12:47:51 +0200

    controller tests Ok

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ var budgetController = (function () {
 
   return {
     publicTest: function (b) {
-      console.log(add(b));
+      // console.log(add(b));
+      return add(b);
     },
   };
 })();
+
+var UIController = (function () {
+  // Some code
+})();
+
+// Connect the controllers: pass the other two modules as arguments
+var controller = (function (budgetCtl, UICtl) {
+  // Some code
+  var z = budgetCtl.publicTest(5);
+
+  return {
+    anotherPublicTest: function () {
+      console.log(z);
+    },
+  };
+})(budgetController, UIController);
