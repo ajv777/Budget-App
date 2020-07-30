79a9eadb69ad804cd9257ffbae6581edbce6a88e
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-30 18:41:28 +0200

    delete event

--- app.js
+++ app.js
@@ -105,6 +105,8 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
+    // Delete item
+    container: ".container",
   };
   return {
     getinput: function () {
@@ -122,11 +124,11 @@ var UIController = (function () {
       if (type === "inc") {
         element = DOMstrings.incomeContainer;
         html =
-          '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i></button> </div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i></button> </div></div></div>';
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div></div>';
       }
       // Replace the text
       newHtml = html.replace("%id%", obj.id);
@@ -183,6 +185,9 @@ var controller = (function (budgetCtl, UICtl) {
         ctlAddItem();
       }
     });
+    document
+      .querySelector(DOM.container)
+      .addEventListener("click", ctlDeleteItem);
   };
 
   var updateBudget = function () {
@@ -222,6 +227,21 @@ var controller = (function (budgetCtl, UICtl) {
 
     // 5. Calculate and update budget
     updateBudget();
+  };
+
+  // Delete an item
+  var ctlDeleteItem = function (event) {
+    var itemIDM, splitID, type;
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    if (itemID) {
+      // split: inc-1
+      splitID = itemIDM.spliptID("-");
+      type = splitID[0];
+      // 1. Delete from the array
+
+      // 2. Delete from the ui
+      // 3. Update and show the new budget
+    }
   };
 
   return {
--- todo.md
+++ todo.md
@@ -1,18 +1,28 @@
 # TO DO LIST
 
-- Create UI Model, Data Model and Controller Model
+## FIRST PART
 
-## UI Model
+- Create UI Model, Data Model and Controller Model:
+
+### UI Model
 
 1. Get input values
 2. Add the new item to the UI
 3. Update the UI
 
-## Data Model
+### Data Model
 
 1. Add the new item to our data structure
 2. Calculate budget
 
-## Controller Model
+### Controller Model
 
 1. Add event handler
+
+## SECOND PART
+
+- Add event handler
+- Delete the item from our data structure
+- Delete the item for the UI
+- Re-calculate the budget
+- Update the UI
