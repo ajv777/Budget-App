be9a2650e947775718feb090dac469a0bfa6dc93
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-30 12:16:49 +0200

    add incomes and expenses and print into the list

--- app.js
+++ app.js
@@ -55,10 +55,14 @@ var budgetController = (function () {
 // UI Controller
 var UIController = (function () {
   var DOMstrings = {
+    // Add
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
     inputBtn: ".add__btn",
+    // Print
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list",
   };
   return {
     getinput: function () {
@@ -68,6 +72,27 @@ var UIController = (function () {
         description: document.querySelector(DOMstrings.inputDescription).value, // item
         value: document.querySelector(DOMstrings.inputValue).value, // $$
       };
+    },
+    // The same object that we created in the budget controller
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+      // Create html string with placeholder text
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i></button> </div></div></div>';
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i> </button> </div> </div></div>';
+      }
+      // Replace the text
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description", obj.description);
+      newHtml = newHtml.replace("%value", obj.value);
+
+      // Insert the HTML into the DOM (as a last child in the list)
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
     // To became in public and use it in the Global Controller
     getDOMstrings: function () {
@@ -104,6 +129,7 @@ var controller = (function (budgetCtl, UICtl) {
     console.log(newItem);
 
     // 3. Add the item to the UI controller
+    UIController.addListItem(newItem, input.type);
     // 4. Calculate the budget
     // 5. Display the budget in the UI
     // console.log("it works");
